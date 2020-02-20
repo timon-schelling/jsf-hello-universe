1fa07f5f270cc8b41287616572d3240716062859
timon.schelling@outlook.de <3417.archi>
2020-02-20 15:41:03 +0100

    make id value public

--- kotlin/de/timokrates/accessgranted/Id.kt
+++ kotlin/de/timokrates/accessgranted/Id.kt
@@ -1,6 +1,6 @@
 package de.timokrates.accessgranted
 
-open class Id(private val value: String) {
+open class Id(val value: String) {
     override fun equals(other: Any?): Boolean {
         if (other === this) return true
         if (other == null) return false
