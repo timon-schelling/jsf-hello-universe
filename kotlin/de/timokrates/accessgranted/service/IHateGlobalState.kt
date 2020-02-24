87e34b684018a73251c939261d0ebfb2b7134f96
timon.schelling@outlook.de <3417.archi>
2020-02-24 08:32:09 +0100

    fix

--- kotlin/de/timokrates/accessgranted/service/IHateGlobalState.kt
+++
@@ -1,5 +0,0 @@
-package de.timokrates.accessgranted.service
-
-import de.timokrates.accessgranted.repository.Repository
-
-object IHateGlobalState : Repository by ServiceRepository()
--- kotlin/de/timokrates/accessgranted/service/Serviceold.kt
+++ kotlin/de/timokrates/accessgranted/service/Serviceold.kt
@@ -1,5 +1,6 @@
 package de.timokrates.accessgranted.service
 
+import de.timokrates.accessgranted.repository.Repository
 import de.timokrates.accessgranted.repository.group.Group
 import de.timokrates.accessgranted.repository.group.GroupId
 import de.timokrates.accessgranted.repository.permissions.Permission
@@ -7,25 +8,25 @@ import de.timokrates.accessgranted.repository.permissions.PermissionId
 import de.timokrates.accessgranted.repository.user.User
 import de.timokrates.accessgranted.repository.user.UserId
 
-object Service {
+object Service : Repository by ServiceRepository() {
 
     object Permissions {
 
         fun add(permissionId: PermissionId) {
-            IHateGlobalState.permissions.find(permissionId) ?: IHateGlobalState.permissions.add(permissionId)
+            permissions.find(permissionId) ?: permissions.add(permissionId)
         }
 
         fun add(permissionId: String) = add(PermissionId(permissionId))
 
-        fun delete(permissionId: PermissionId) = IHateGlobalState.permissions.remove(permissionId)
+        fun delete(permissionId: PermissionId) = permissions.remove(permissionId)
 
         fun delete(permissionId: String) = delete(PermissionId(permissionId))
 
-        fun find(permissionId: PermissionId) = IHateGlobalState.permissions.find(permissionId)
+        fun find(permissionId: PermissionId) = permissions.find(permissionId)
 
         fun find(permissionId: String) = find(PermissionId(permissionId))
 
-        fun all(): List<Permission> = IHateGlobalState.permissions.map { it }
+        fun all(): List<Permission> = permissions.map { it }
 
         fun allIds(): List<PermissionId> = all().map { it.id }
 
@@ -34,20 +35,20 @@ object Service {
     object Users {
 
         fun add(userId: UserId) {
-            IHateGlobalState.users.find(userId) ?: IHateGlobalState.users.add(userId)
+            users.find(userId) ?: users.add(userId)
         }
 
         fun add(userId: String) = add(UserId(userId))
 
-        fun delete(userId: UserId) = IHateGlobalState.users.remove(userId)
+        fun delete(userId: UserId) = users.remove(userId)
 
         fun delete(userId: String) = delete(UserId(userId))
 
-        fun find(userId: UserId) = IHateGlobalState.users.find(userId)
+        fun find(userId: UserId) = users.find(userId)
 
         fun find(userId: String) = find(UserId(userId))
 
-        fun all(): List<User> = IHateGlobalState.users.map { it }
+        fun all(): List<User> = users.map { it }
 
         fun allIds(): List<UserId> = all().map { it.id }
 
@@ -72,20 +73,20 @@ object Service {
     object Groups {
 
         fun add(groupId: GroupId) {
-            IHateGlobalState.groups.find(groupId) ?: IHateGlobalState.groups.add(groupId)
+            groups.find(groupId) ?: groups.add(groupId)
         }
 
         fun add(groupId: String) = add(GroupId(groupId))
 
-        fun delete(groupId: GroupId) = IHateGlobalState.groups.remove(groupId)
+        fun delete(groupId: GroupId) = groups.remove(groupId)
 
         fun delete(groupId: String) = delete(GroupId(groupId))
 
-        fun find(groupId: GroupId) = IHateGlobalState.groups.find(groupId)
+        fun find(groupId: GroupId) = groups.find(groupId)
 
         fun find(groupId: String) = find(GroupId(groupId))
 
-        fun all(): List<Group> = IHateGlobalState.groups.map { it }
+        fun all(): List<Group> = groups.map { it }
 
         fun allIds(): List<GroupId> = all().map { it.id }
 
