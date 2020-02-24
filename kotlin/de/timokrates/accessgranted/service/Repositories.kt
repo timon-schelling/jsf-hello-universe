0324cb6eb59ea0c530afa2fbb78e6a1ee35c2865
timon.schelling@outlook.de <3417.archi>
2020-02-24 13:46:38 +0100

    post review

--- kotlin/de/timokrates/accessgranted/service/Repositories.kt
+++ kotlin/de/timokrates/accessgranted/service/Repositories.kt
@@ -1,11 +1,11 @@
-package de.timokrates.accessgranted.repository
+package de.timokrates.accessgranted.service
 
 import de.timokrates.accessgranted.repository.group.GroupRepository
 import de.timokrates.accessgranted.repository.permissions.PermissionRepository
 import de.timokrates.accessgranted.repository.token.TokenRepository
 import de.timokrates.accessgranted.repository.user.UserRepository
 
-interface Repository {
+interface Repositories {
     val tokens: TokenRepository
     val users: UserRepository
     val groups: GroupRepository
--- kotlin/de/timokrates/accessgranted/service/ServiceRepository.kt
+++ kotlin/de/timokrates/accessgranted/service/ServiceRepository.kt
@@ -1,8 +1,6 @@
 package de.timokrates.accessgranted.service
 
-import de.timokrates.accessgranted.repository.Repository
-
-class ServiceRepository : Repository {
+class ServiceRepository : Repositories {
     override val tokens by lazy { ListTokenRepository() }
     override val users by lazy { ListUserRepository() }
     override val groups by lazy { ListGroupRepository() }
